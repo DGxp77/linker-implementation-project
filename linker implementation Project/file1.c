8792bea1009998757c477cc3f341469b06987a8b
DGxp77 <88278867+DGxp77@users.noreply.github.com>
2021-08-01 19:31:18 +0530

    Initial commit

---
+++ linker implementation Project/file1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+void second(void);
+int main() 
+{
+	printf("This statement is from the main program");
+	second();
+	return 0;
+}
---
+++ linker implementation Project/file2.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+void second(void) 
+{
+	printf("\n this print statement is from the second program !");
+}
